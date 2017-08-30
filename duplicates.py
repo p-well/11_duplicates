660f89a1f3158c3b0362d986ddbbc6c7a15d692e
Pavel <p.a.kadantsev@gmail.com>
2017-08-30 22:55:37 +0300

    Add dublicates search func and print

--- duplicates.py
+++ duplicates.py
@@ -7,12 +7,26 @@ def get_files_list(folderpath):
             filepath = os.path.join(dirpath, file)
             filesize = os.path.getsize(filepath)
             files_list.append((dirpath, file, filesize))
-    print(files_list)
+    return files_list
 
 def find_dublicates(files_list):
     dublicated_files = []
     non_dublicated_files = []
-
-
+    for dirpath, file, filesize in files_list:
+        if (file, filesize) in non_dublicated_files:
+            dublicated_files.append(os.path.join(dirpath, file))
+        else:
+            non_dublicated_files.append((file, filesize))
+    return dublicated_files
+	
 if __name__ == '__main__':
-    all_files_in_tree = get_files_list('C:\projects\ptest_folder')
+    
+    all_files_in_tree = get_files_list('C:\')
+    dublicated_files = find_dublicates(all_files_in_tree)
+	
+    if len(dublicated_files) == 0:
+        print('No dublicated files have been found in this directory tree.')
+    else:
+        print('The following files are dublicates:')
+        for dubl_file in dublicated_files:
+            print('Location: {} File name: {}'.format(dubl_file[0], dubl_file[1]))
