10e71576541689fd8e5d74ad54437d7cb7525fce
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-08-30 13:49:20 +0300

    Add get files list func

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,18 @@
+import os
+
+def get_files_list(folderpath):
+    files_list = []
+    for dirpath, subdirs, files in os.walk(folderpath):
+        for file in files:
+            filepath = os.path.join(dirpath, file)
+            filesize = os.path.getsize(filepath)
+            files_list.append((dirpath, file, filesize))
+    print(files_list)
+
+def find_dublicates(files_list):
+    dublicated_files = []
+    non_dublicated_files = []
+
 
 if __name__ == '__main__':
-    pass
+    all_files_in_tree = get_files_list('C:\projects\ptest_folder')
