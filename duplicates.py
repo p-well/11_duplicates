296f5405c44484b0dcea3d2958e5bd8672a68007
Pavel <p.a.kadantsev@gmail.com>
2017-08-31 20:17:45 +0300

    Add argparser

--- duplicates.py
+++ duplicates.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 def get_files_list(folderpath):
     files_list = []
@@ -18,15 +19,24 @@ def find_dublicates(files_list):
         else:
             non_dublicated_files.append((file,filesize))
     return dublicated_files
+
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path_to_dir', nargs='?', default = current_dir_path, type=argparse.FileType())
+    parsed_dirpath = parser.parse_args()
+    return parsed_dirpath
 	
 if __name__ == '__main__':
-    
-    all_files_in_tree = get_files_list('C:\projects\ptest_folder')
+
+    current_dir_path = os.getcwd() 
+    parsed_dirpath = createParser()
+    all_files_in_tree = get_files_list(parsed_dirpath.path_to_dir)
     dublicated_files = find_dublicates(all_files_in_tree)
-	
+    
     if len(dublicated_files) == 0:
-        print('No dublicated files have been found in this directory tree.')
+        print('\nNo dublicated files have been found in this directory tree.')
     else:
         print('\nThe following files are dublicates: \n')
         for dubl_file in dublicated_files:
-            print('Location:{}\tFile:{}'.format(dubl_file[0], dubl_file[1]))
+            print('Location: {}\tFile: {}'.format(dubl_file[0], dubl_file[1]))
+            
