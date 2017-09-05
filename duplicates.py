d99b77a4a2d045465b7a03f120fb1512e0d33f6d
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-09-05 21:45:05 +0300

    Add print func, remove os.getcwd, remove default arg from argparse

--- duplicates.py
+++ duplicates.py
@@ -20,35 +20,28 @@ def find_dublicates(files_list):
             non_dublicated_files.append((file,filesize))
     return dublicated_files
 
-def createParser():
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path_to_dir')
+    dirpath = parser.parse_args()
+    return dirpath
+    
+def print_results(dublicated_files):
+    if not dublicated_files:
+        print('\nNo dublicated files have been found in this directory tree.')
+    else:
+        print('\nThe following files are dublicates: \n')
+        for dubl_file in dublicated_files:
+            print('Location: {}\tFile: {}'.format(dubl_file[0], dubl_file[1]))
 
-    parser = argparse.ArgumentParser(prog = 'Search for duplicates',
-                                     description = '''The script searches for dublicated files
-in current working directory or any other directory (need to specify its absolute path). ''',
-                                     epilog = '''(c) p-well 2017. The author of the program 
-does not bear any responsibility.''')
-
-    parser.add_argument('path_to_dir', nargs='?', default = current_dir_path)
-    parsed_dirpath = parser.parse_args()
-    return parsed_dirpath
-	
-if __name__ == '__main__':
-
-    current_dir_path = os.getcwd() 
-    parsed_dirpath = createParser()
-
-    if not os.path.isdir(parsed_dirpath.path_to_dir):
-
-        print('\nNo such directory, check path and try again')
-
-    else:  
-
-        all_files_in_tree = get_files_list(parsed_dirpath.path_to_dir)
+def main():
+    dirpath = create_parser()
+    if os.path.exists(dirpath.path_to_dir):
+        all_files_in_tree = get_files_list(dirpath.path_to_dir)
         dublicated_files = find_dublicates(all_files_in_tree)
-        
-        if len(dublicated_files) == 0:
-            print('\nNo dublicated files have been found in this directory tree.')
-        else:
-            print('\nThe following files are dublicates: \n')
-            for dubl_file in dublicated_files:
-                print('Location: {}\tFile: {}'.format(dubl_file[0], dubl_file[1]))
+        print_results(dublicated_files)
+    else:  
+        print('\nInvalid path, try again')        
+
+if __name__ == '__main__':
+    main()
